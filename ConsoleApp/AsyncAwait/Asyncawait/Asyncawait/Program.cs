e3a78b35b69de6e14062d47e09fae2b7fdbbb5a9
jagadeshwaran <76894794+jagadeshwarandev@users.noreply.github.com>
2022-01-03 15:40:28 -0500

    03-01-21

---
+++ ConsoleApp/AsyncAwait/Asyncawait/Asyncawait/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Asyncawait
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Firstline();
+            ThirdLine(3, 3);
+            Secondline();
+            
+            Console.ReadKey();
+            async Task Firstline ()
+            {
+                Console.WriteLine("Frist Line");
+            }
+            async Task Secondline()
+            {
+                Console.WriteLine("Second Line");
+
+            }
+            async Task ThirdLine(int x,int y)
+            {
+                int c;
+                c = x + y;
+                await Task.Delay(1000);
+
+                Console.WriteLine(c);
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp/Serialization/SerialandDeserialization/SerialandDeserialization/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.IO;
+
+
+namespace SerialandDeserialization
+{
+    [Serializable]
+    class Tutorial
+    {
+        public int ID;
+        public string Name;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tutorial obj = new Tutorial();
+            obj.ID = 1;
+            obj.Name = ".NET";
+
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(@"D:\Serial.txt", FileMode.Create, FileAccess.Write);
+            formatter.Serialize(stream, obj);
+            stream.Close();
+
+            stream =new FileStream(@"D:\serial.txt", FileMode.Open, FileAccess.Read);
+            Tutorial objnew = (Tutorial)formatter.Deserialize(stream);
+            Console.WriteLine(objnew.ID);
+            Console.WriteLine(objnew.Name);
+            Console.ReadKey();
+
+
+        }
+    }
+}
